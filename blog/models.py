4be0556044a52719ffd2d2e1f47d5aafc0d56b0e
Pauline Rugwevera <prugwevera28@gmail.com>
2022-06-16 12:21:31 +0000

    Create database models

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+
+class Profile:
+    user = models.OneToOneField(User, on_delete=models.CASCADE, blank=True, null=True)
+    image = CloudinaryField('image', default='placeholder')
+    bio = models.TextField(blank=True, null=True)
+    linkedin = models.CharField(max_length=300, blank=True, null=True)
+    Twitter = models.CharField(max_length=300, blank=True, null=True)
+
+    def __str__(self):
+        return str(self.user)
+
+
+class BlogPost(models.Model):
+    title = models.CharField(max_length=250, unique=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    slug = models.CharField(max_length=150)
+    content = models.TextField()
+    image = CloudinaryField('image', default='placeholder')
+    blog_post_id = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return str(self.author) + " Blog Title: " + self.title
+
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    blog_id = models.ForeignKey(BlogPost, on_delete=models.CASCADE,related_name="comments")
+
+    def __str__(self):
+        return self.title
+
+
+   
+   

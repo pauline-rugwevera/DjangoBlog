aa2a81881f5b95f87037460e6d8ec5573035c187
Pauline Rugwevera <prugwevera28@gmail.com>
2022-07-20 14:25:47 +0000

    depolyment commit

--- webdev/settings.py
+++ webdev/settings.py
@@ -30,6 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+X_FRAME_OPTIONS = 'SAMEORIGIN'
 
 ALLOWED_HOSTS = ["webdev-django-blog.herokuapp.com", "localhost"]
 
